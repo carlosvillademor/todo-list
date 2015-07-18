eecb7f91b4d67b8b174e2b84a1cd2a581d2e0b18
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2015-07-18 12:17:05 +0100

    Refactor route handling

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,11 +1,15 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as jetty]))
 
+(defn welcome
+  "A ring handler to process all requests sent to the webapp"
+  [request]
+  {:status 200
+   :body "<h1>Hello, Clojure World</h1>  <p>Welcome to your first Clojure app.  This message is returned regardless of the request, sorry<p>"
+   :headers {}})
+
 (defn -main
   "A very simple web server using Ring & Jetty"
   [port-number]
-  (jetty/run-jetty
-     (fn [request] {:status 200
-                   :body "<h1>Hello, Clojure World</h1>  <p>Welcome to your first Clojure app.  This message is returned regardless of the request, sorry<p>"
-                   :headers {}})
-     {:port (Integer. port-number)}))
+  (jetty/run-jetty welcome
+    {:port (Integer. port-number)}))
