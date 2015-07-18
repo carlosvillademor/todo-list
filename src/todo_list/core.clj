20edff7dcd8505865c210afd8952e54a30dac932
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2015-07-18 14:01:45 +0100

    Adding a bunch of handlers

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -2,19 +2,76 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found]])
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]])
   (:gen-class))
 
-(defn welcome
+(defn greet
   "A ring handler to process all requests sent to the webapp"
   [request]
   {:status 200
    :body "<h1>Hi people, again</h1>  <p>Welcome to your first Clojure app.  This message is returned regardless of the request, sorry<p>"
    :headers {}})
 
+(defn goodbye
+  "A song to wish you goodbye"
+  [request]
+  {:status 200
+   :body "<h1>Walking back to happiness</h1>
+          <p>Walking back to happiness with you</p>
+          <p>Said, Farewell to loneliness I knew</p>
+          <p>Laid aside foolish pride</p>
+          <p>Learnt the truth from tears I cried</p>"
+   :headers {}})
+
+(defn about
+  "Information about the website developer"
+  [request]
+  {:status 200
+   :body "I am an awesome Clojure developer, well getting there..."
+   :headers {}})
+
+(defn request-info
+  "View the information contained in the request, useful for debugging"
+  [request]
+  {:status 200
+   :body (pr-str request)
+   :headers {}})
+
+(defn hello
+  "A simple personalised greeting showing the use of variable parth elements"
+  [request]
+  (let [name (get-in request [:route-params :name])]
+    {:status 200
+     :body (str "Hello " name ".  I got your name from the web URL")
+     :headers {}}))
+
+(def operands {"+" + "-" - "*" * ":" /})
+
+(defn calculator
+  "A very simple calculator that can add, divide, subtract and multiply.  This is done through the magic of variable path elements."
+  [request]
+  (let [a  (Integer. (get-in request [:route-params :a]))
+        b  (Integer. (get-in request [:route-params :b]))
+        op (get-in request [:route-params :op])
+        f  (get operands op)]
+    (if f
+      {:status 200
+       :body (str (f a b))
+       :headers {}}
+      {:status 404
+       :body "Sorry, unknown operator.  I only recognise + - * : (: is for division)"
+       :headers {}})))
+
 (defroutes app
-  (GET "/" [] welcome)
-  (not-found "<h1>This is not the page you are looking for</h1> <p>Sorry, the page you requested was not found!</p>"))
+  (GET "/" [] greet)
+  (GET "/goodbye" [] goodbye)
+  (GET "/about" [] about)
+  (GET "/handle-dump" [] handle-dump)
+  (GET "/request-info" [] request-info)
+  (GET "/hello/:name" [] hello)
+  (GET "/calculator/:a/:op/:b" [] calculator)
+  (not-found "Sorry, page not found"))
 
 (defn -main
   "A very simple web server using Ring & Jetty"
