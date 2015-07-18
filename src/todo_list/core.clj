61b143218283bdc55ca5ac82eebf84dd6241249c
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2015-07-18 12:37:31 +0100

    Adding compojure and some defroutes

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,6 +1,8 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]))
 
 (defn welcome
   "A ring handler to process all requests sent to the webapp"
@@ -9,14 +11,18 @@
    :body "<h1>Hi people, again</h1>  <p>Welcome to your first Clojure app.  This message is returned regardless of the request, sorry<p>"
    :headers {}})
 
+(defroutes app
+  (GET "/" [] welcome)
+  (not-found "<h1>This is not the page you are looking for</h1> <p>Sorry, the page you requested was not found!</p>"))
+
 (defn -main
   "A very simple web server using Ring & Jetty"
   [port-number]
-  (jetty/run-jetty welcome
+  (jetty/run-jetty app
     {:port (Integer. port-number)}))
 
 (defn -dev-main
   "A very simple web server using Ring & Jetty that reloads code changes via the development profile of Leiningen"
   [port-number]
-  (jetty/run-jetty (wrap-reload #'welcome)
+  (jetty/run-jetty (wrap-reload #'app)
      {:port (Integer. port-number)}))
